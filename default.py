b0b3922680531241bd371a0da27228f34490500e
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2012-09-02 16:42:14 -0500

    Se implementa la descarga y dialogo de espera.
    Se crean clases de descarga y dialogo de espera para que dichas
    actividades se ejecuten de manera paralela, ademas se utilizan en el
    código.
    Sin embargo, ahora se va a cambiar la lógica de programación ya que los
    archivos están completos, no ay q cargarlos y descargarlos.

--- default.py
+++ default.py
@@ -12,7 +12,11 @@ from urllib2 import Request, urlopen, URLError, HTTPError
 import logging
 from HTMLParser import HTMLParser
 import shelve
-
+import math
+import threading  
+import random
+import socket
+import time
 ############VERSION#############
 
 version = "0.1"
@@ -54,7 +58,75 @@ class ParserHTML(HTMLParser):
                 self.carpetas[carpeta] = {}
             self.carpetas[carpeta][archivo] = attrs[0][1]
             
-            
+class HiloDialogo(threading.Thread):  
+    logger = None
+    def __init__(self, time_to_wait, title, text):  
+          threading.Thread.__init__(self)  
+          logging.basicConfig(level=logging.DEBUG)
+          logger = logging.getLogger("HiloDialogo")
+          logger.debug("Aventanto dialogo")
+          self.time_to_wait = time_to_wait
+          self.title = title
+          self.text = text
+
+    
+    def run(self):  
+            logger.debug('esperando ' + str(self.time_to_wait) + ' segs')
+    
+            pDialog = xbmcgui.DialogProgress()
+            ret = pDialog.create(self.title)
+    
+            secs = 0
+            percent = 0
+            increment = 100 / self.time_to_wait
+    
+            cancelled = False
+            while secs < self.time_to_wait:
+                secs = secs + 1
+                percent = increment * secs
+                secs_left = str((self.time_to_wait - secs))
+                remaining_display = ' Espere ' + secs_left + ' segs descargando video...'
+                pDialog.update(percent, ' ' + self.text, remaining_display)
+                xbmc.sleep(1000)
+                if (pDialog.iscanceled()):
+                     cancelled = True
+                     break
+    
+            if cancelled == True:
+                 logger.debug('espera cancelada')
+            else:
+                 logger.debug('Fin de la espera')
+             
+class HiloDescarga(threading.Thread):  
+    logger = None
+
+    def __init__(self, url_a_bajar):  
+          threading.Thread.__init__(self)  
+          logging.basicConfig(level=logging.DEBUG)
+          logger = logging.getLogger("HiloDescarga")
+          logger.debug("Aventanto hilo de descarga")
+          HiloDescarga.logger = logger
+          self.url_a_bajar = url_a_bajar
+    
+    def run(self):  
+        nombre_archivo = ""
+        intentos = 0
+        tiempo_dormir = 0
+        logger = HiloDescarga.logger
+        logger.debug("DEscargando archivo " + self.url_a_bajar)
+        
+        while (nombre_archivo == "" and intentos < 3):
+            logger.debug("Intentando bajara archuvi %s" % self.url_a_bajar)
+            try:
+                nombre_archivo = urllib.urlretrieve(self.url_a_bajar)[0]
+                logger.debug("El archivo %s se ha descargado " % nombre_archivo)
+            except IOError as ioe:
+                logger.warn("Hubo un error de conexion %s a %s, reintentando" % (self.url_a_bajar, ioe))
+            if(nombre_archivo == ""):
+                tiempo_dormir = random.randint(1, 10)
+                logger.warn("El hilo se dormira %d segundos para volver a intentar " % tiempo_dormir)
+                time.sleep(tiempo_dormir)
+                intentos += 1
 ####Funciones
                 
 def get_params():
@@ -74,13 +146,102 @@ def get_params():
                                 param[splitparams[0]] = splitparams[1]
                                 
         return param
-      
-        
+ 
+ 
+ 
+def dialogo_loading(time_to_wait, title, text):
+        logger.debug('esperando ' + str(time_to_wait) + ' segs')
+
+        pDialog = xbmcgui.DialogProgress()
+        ret = pDialog.create(title)
+
+        secs = 0
+        percent = 0
+        increment = 100 / time_to_wait
+
+        cancelled = False
+        while secs < time_to_wait:
+            secs = secs + 1
+            percent = increment * secs
+            secs_left = str((time_to_wait - secs))
+            remaining_display = ' Espere ' + secs_left + ' segs descargando video...'
+            pDialog.update(percent, ' ' + text, remaining_display)
+            xbmc.sleep(1000)
+            if (pDialog.iscanceled()):
+                 cancelled = True
+                 break
+
+        if cancelled == True:
+             logger.debug('espera cancelada')
+             return False
+        else:
+             logger.debug('Fin de la espera')
+             return True
+
+def calcular_velocidad_internet():
+    URL_PAG_VELOCIDAD = "http://testmy.net/results?align=&autoXi=&autoXiUL=&custom_packsize=&maxDFS=&maxUFS=&minDFS=&minUFS=&nfw=&noLoad=&noads=&nobar=&nobar=&out_size=&out_src=&out_url=&randGen=&smarTest=&special=1&ta=&test_type=download&test_type=download&testall=0&top=&ttime=Wed%20Aug%2029%202012%2023%3A43%3A58%20GMT-0500%20%28CDT%29&username=&x=5.072&xMes=2&z=3145728"
+    velocidad = 0
+    velocidad_str = ""
+    lineas_pag_velocidad = []
+    lineas_pag_velocidad = urllib.urlopen(URL_PAG_VELOCIDAD).read().split("\n")
+    for linea_pag_velocidad in lineas_pag_velocidad:
+#        logger.debug("Me corto las pelotas " + linea_pag_velocidad)
+        if ("Your Speed ::" in linea_pag_velocidad):
+            velocidad_str = re.sub(r".*Your Speed :: <span class=\"containerIndexX\"><font class=\"color\d+\">(\d+\.?\d*)</font>.*", r"\1", linea_pag_velocidad)
+#                                      Your Speed :: <span class="containerIndexX"><font class="color22">5</font>
+            logger.debug("La velocidad de internet es " + velocidad_str)
+            velocidad = float(velocidad_str)
+            velocidad *= 1000000
+            break
+    velocidad /= 8
+    return velocidad
+
+def calcular_total_a_bajar(url_descargas):
+    total = 0
+    tamano_archivo_txt = ""
+    digitos_txt = ""
+    factor_multiplicativo = ""
+    tamano_archivo = 0
+    lineas_pag_descargables = []
+    lineas_pag_descargables = urllib.urlopen(url_descargas).read().split("\n")
+    for linea_pag_descargables in lineas_pag_descargables:
+#        logger.debug("La linea de descargables es " + linea_pag_descargables)
+        if ("<tr><td valign=" in linea_pag_descargables and not "DIR" in linea_pag_descargables):
+            tamano_archivo_txt = re.sub(r".*<td align=\"right\">\s*(\d+\.?\d*[MKG]?)\s*</td>.*", r"\1", linea_pag_descargables)
+            logger.debug("El tamano del archivo %s", tamano_archivo_txt)
+            (digitos_txt, factor_multiplicativo) = re.sub(r"(\d+\.?\d*)([MKG]?)", r"\1:\2", tamano_archivo_txt).split(":")
+            tamano_archivo = float(digitos_txt)
+            if(factor_multiplicativo == "K"):
+                tamano_archivo *= 1000
+            elif(factor_multiplicativo == "M"):
+                tamano_archivo *= 1000000
+            elif(factor_multiplicativo == "G"):
+                tamano_archivo *= 1000000000
+            else:
+                tamano_archivo = tamano_archivo
+            total += tamano_archivo
+            logger.debug("El total en bytes %f" % tamano_archivo)
+         
+    return total
+
+def get_urls_archivos(url_descargas):
+    urls_archivos = []
+    url_archivo = ""
+    lineas_pag_descargables = []
+    lineas_pag_descargables = urllib.urlopen(url_descargas).read().split("\n")
+    for linea_pag_descargables in lineas_pag_descargables:
+        if ("<tr><td valign=" in linea_pag_descargables and not "DIR" in linea_pag_descargables):
+            url_archivo = re.sub(r".*href=\"(.+)\".*</a>.*", r"\1", linea_pag_descargables)
+            logger.debug("La url del archivo es " + url_archivo)
+            urls_archivos.append(url_archivo)
+    return urls_archivos
+
 ####Constantes
-REMOTE_DBG = True
+REMOTE_DBG = False
 URL_CACA = "http://peliculas.mine.nu:8080/TV-XVID/"
 MODO_CARPETA = 0
 MODO_ARCHIVO = 1
+MODO_REPRODUCIR = 2
 
 ####Variables
 #Inicializadas
@@ -89,14 +250,19 @@ params = get_params()
 url_plugin = sys.argv[0]
 handle_xbmc = int(sys.argv[1])
 #Sin inicializar
-lineas_pagina = ()
+velocidad_internet = 0
+total_a_descargar = 0
+urls_archivos_descargables = []
+hilos_archivos_descargables = []
 logger = None
 opener = None
 pagina = None
 parser_html = None
 carpeta_li = None
 archivo_persistencia = None
+url_descarga = ""
 carpetas = {}
+hilo_dialogo = None
 # append pydev remote debugger
 if REMOTE_DBG:
     # Make pydev debugger works for auto reload.
@@ -115,6 +281,9 @@ if REMOTE_DBG:
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("mierda")
 logger.debug("kussou rumba %s" % params)
+
+#Configurando el timeout de los socketes
+socket.setdefaulttimeout(60)
 
 if(len(params) > 0 and params["modo"]):
     modo = int(params["modo"], 0)
@@ -135,7 +304,10 @@ if (modo == MODO_CARPETA):
          carpeta_li = xbmcgui.ListItem(carpeta.replace(".", " "), iconImage="DefaultFolder.png", thumbnailImage="")
          carpeta_li.setInfo(type="Video", infoLabels={ "Title": carpeta.replace(".", " ") })
          xbmcplugin.addDirectoryItem(handle=handle_xbmc, url="%s?carpeta=%s&modo=%d" % (url_plugin, carpeta, MODO_ARCHIVO) , listitem=carpeta_li, isFolder=True)
-else:
+
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+    
+elif (modo == MODO_ARCHIVO):
     archivo_persistencia = shelve.open("mierda")
     carpetas = archivo_persistencia["carpetas"] 
     
@@ -144,9 +316,35 @@ else:
         carpeta_li = xbmcgui.ListItem(archivo, iconImage="DefaultFolder.png", thumbnailImage="")
         carpeta_li.setInfo(type="Video", infoLabels={ "Title": archivo })
         logger.debug("La url a la que e hara la peticion es " + URL_CACA + params["carpeta"])
-#        xbmcplugin.addDirectoryItem(handle=handle_xbmc, url=URL_CACA + params["carpeta"] , listitem=carpeta_li, isFolder=False)
-        xbmcplugin.addDirectoryItem(handle=handle_xbmc, url="http://peliculas.mine.nu:8080/TV-XVID/The.Simpsons.S22E17.HDTV.XviD-LOL/Sample/the.simpsons.2217.hdtv-lol-sample.avi" , listitem=carpeta_li, isFolder=False)
+        xbmcplugin.addDirectoryItem(handle=handle_xbmc, url="%s?carpeta=%s&archivo=%s&modo=%d" % (url_plugin, params["carpeta"] , archivo, MODO_REPRODUCIR) , listitem=carpeta_li, isFolder=False)
+    
+    archivo_persistencia.close()
+   
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))   
+else:
+    archivo_persistencia = shelve.open("mierda")
+    carpetas = archivo_persistencia["carpetas"]
+    
+    logger.debug("La carpeta es %s, el archivo es %s" % (params["carpeta"], params["archivo"]))
+    url_descarga = URL_CACA + carpetas[params["carpeta"]][params["archivo"]]
     
     archivo_persistencia.close()
     
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
+    
+    logger.debug("Modo de reproduccion la url %s" % url_descarga)
+    velocidad_internet = calcular_velocidad_internet()
+    logger.debug("La velocidad de internet es %d" % velocidad_internet)
+    total_a_descargar = calcular_total_a_bajar(url_descarga)
+    logger.debug("El total a descargar es %d", total_a_descargar)
+    urls_archivos_descargables = get_urls_archivos(url_descarga)
+    
+    hilo_dialogo = HiloDialogo(math.ceil(total_a_descargar / velocidad_internet + ((total_a_descargar * 0.1) / velocidad_internet)), "mierda", "caca")
+    hilo_dialogo.start()
+    
+    #dialogo_loading(math.ceil(total_a_descargar / velocidad_internet + ((total_a_descargar * 0.1) / velocidad_internet)), "mierda", "caca")
+    logger.debug("Empieza la descarga de poder")
+    for url_archivo_descargable in urls_archivos_descargables:
+        hilos_archivos_descargables.append(HiloDescarga(url_descarga + url_archivo_descargable))
+        hilos_archivos_descargables[-1].start()
+    
+
